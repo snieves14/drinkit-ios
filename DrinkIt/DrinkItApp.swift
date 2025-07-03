244e3d4e56078ae54a63a819c45d145940a9cd03
snieves14 <snieves14@gmail.com>
2025-07-03 15:51:34 +0200

    first commit

---
+++ DrinkIt/DrinkItApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkItApp.swift
+//  DrinkIt
+//
+//  Created by Santi Nieves on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DrinkItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# drinkit-ios
+
+* Xcode 16.4+
+* SwiftUI
+
+## Getting Started
+
+1. Clone the repository to your local machine.
+
+## Swift Package Manager (SPM)
+
+
+## Desing pattern
+
+The design pattern chosen for this project has been MV.
